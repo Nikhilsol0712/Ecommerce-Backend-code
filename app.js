48e7b88205545310eb175ec90bfa4e3edfdac76b
Nikhilsol0712 <solankiniks0712@gmail.com>
2022-12-15 12:22:49 +0530

    add cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const categoryRoutes = require("./routes/category.routes");
 const productRoutes = require("./routes/product.routes");
 const authRoutes = require("./routes/auth.routes");
@@ -11,6 +12,8 @@ const app = express();
 
 /* app.use() is using the provided middleware for every incoming request to the server*/
 /* We need to a body parser middleware, that will help express to read all the query and body params */
+app.use(cors());
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 categoryRoutes(app);
