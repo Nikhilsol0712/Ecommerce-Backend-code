77bf497d06b7c97de446a0fe17fcbcbbde5ceb98
Nikhilsol0712 <solankiniks0712@gmail.com>
2022-12-17 11:16:22 +0530

    change variable name in secret key

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 
 const serverPORT = process.env.PORT;
 const S3_BUCKET = process.env.S3_BUCKET;
-const SECRET_KEY = process.env.SECRET_KEY;
+const JWT_SECRET_KEY = process.env.SECRET_KEY;
 
-module.exports = { PORT: serverPORT, SECRET_KEY, S3_BUCKET };
+module.exports = { PORT: serverPORT, JWT_SECRET_KEY, S3_BUCKET };
