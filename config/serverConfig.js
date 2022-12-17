c99d0f7bbce3ff8e6d6fa6cfdb6fa5916f960826
Nikhilsol0712 <solankiniks0712@gmail.com>
2022-12-17 11:02:30 +0530

    add secret key

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -1,5 +1,7 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const serverPORT = process.env.PORT;
+const S3_BUCKET = process.env.S3_BUCKET;
+const SECRET_KEY = process.env.SECRET_KEY;
 
-module.exports = {PORT: serverPORT}
+module.exports = { PORT: serverPORT, SECRET_KEY, S3_BUCKET };
